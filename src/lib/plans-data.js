e8447c18724f209fa55f542bbce68c104d9bc24a
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-27 15:43:44 +0100

    Add investment plans data to plans-data.js

---
+++ src/lib/plans-data.js
@@ -0,0 +1,12 @@
+export const investmentPlans = [
+    { id: 'VIP1', name: 'VIP 1', min: 2800, daily: 650, days: 20, total: 13000, limit: 1 },
+    { id: 'VIP2', name: 'VIP 2', min: 3600, daily: 850, days: 20, total: 17000, limit: 1 },
+    { id: 'VIP3', name: 'VIP 3', min: 4500, daily: 1000, days: 20, total: 20000, limit: 1 },
+    { id: 'VIP4', name: 'VIP 4', min: 6000, daily: 1300, days: 20, total: 26000, limit: 1 },
+    { id: 'VIP5', name: 'VIP 5', min: 10000, daily: 1700, days: 20, total: 34000, limit: 1 },
+    { id: 'VIP6', name: 'VIP 6', min: 15000, daily: 2000, days: 20, total: 40000, limit: 1 },
+    { id: 'VIP7', name: 'VIP 7', min: 20000, daily: 2400, days: 20, total: 48000, limit: 1 },
+    { id: 'VIP8', name: 'VIP 8', min: 30000, daily: 3500, days: 20, total: 70000, limit: 1 },
+    { id: 'VIP9', name: 'VIP 9', min: 50000, daily: 6000, days: 20, total: 120000, limit: 1 },
+    { id: 'VIP10', name: 'VIP 10', min: 100000, daily: 13000, days: 20, total: 260000, limit: 1 },
+];
