ca5e2493c24e8a154d21d0cf2fb81e3928990fd8
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-27 15:39:48 +0100

    Add currency and date formatting utilities

---
+++ src/utils/format.js
@@ -0,0 +1,17 @@
+export const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-NG', {
+        style: 'currency',
+        currency: 'NGN',
+        minimumFractionDigits: 2
+    }).format(amount || 0);
+};
+
+export const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleDateString('en-NG', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+};
